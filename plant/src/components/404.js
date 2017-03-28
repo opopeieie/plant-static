c0ff5226181ff82afd7a41e2110696ffa965476b
Oscar.chen <ChenXinqiao@chinasofti.com>
2017-03-28 11:26:10 +0800

    tesetMergeMaster

--- plant/src/components/404.js
+++ plant/src/components/404.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 export default class NotFound extends Component {
     static contextTypes = {
-        router: React.PropTypes.object.isRequired
+        router: React.PropTypes.object.isRequired,
+        bbb:'213'
     }
 
     componentWillMount() {
         alert('404 NOT FOUND')
         this.context.router.replace('/')
+        console.log('tesetBranch');
     }
 
     render () {
